8523a63c816c4c7c255bc7c794a0d0ab64113836
deltaryz <wincam97@gmail.com>
2021-06-10 23:38:00 -0500

    make terraria work on raspi

--- terraria/start.sh
+++ terraria/start.sh
@@ -3,7 +3,7 @@
 while :
 do
   docker-compose down # make sure we clean up our junk
-  docker-compose run -p 7777:7777 terraria
   echo "press CTRL-C to cancel restarting..."
   sleep 5
+  docker-compose run -p 7777:7777 terraria
 done
