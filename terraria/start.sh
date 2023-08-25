6a408cc91c234c00fe00b82c1f2db4e77f034b54
deltaryz <wincam97@gmail.com>
2023-08-25 07:08:16 -0500

    cleanup old files

--- terraria/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-while :
-do
-  docker-compose down # make sure we clean up our junk
-  echo "press CTRL-C to cancel restarting..."
-  sleep 5
-  docker-compose run -p 7777:7777 terraria
-done
